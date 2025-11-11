33bf5a77fef09a366bff8dc9b25668b1d905b623
Nqnt41 <ryanpcoveny@gmail.com>
2025-11-11 01:36:00 -0500

    Completed final tasks for project, bug-fixing and made sure requirements matched.

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -12,7 +12,6 @@ function ProductList({ onHomeClick }) {
 
     const cart = useSelector(state => state.cart.items);
 
-
     const dispatch = useDispatch();
 
     const plantsArray = [
@@ -295,31 +294,36 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
-                    <div className='cart_quantity_count'>{quantity}</div>
+                    <div> <div className='cart_quantity_count' style={{ cursor: 'pointer' }} onClick={(e) => handleCartClick(e)}>{quantity}</div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                   
                 </div>
             </div>
             {!showCart ? (
                 <div className="product-grid">
                     {plantsArray.map((group, outerIndex) => (
-                        <div className="product-list">
-                            <h1>{group.category}</h1>
-                            {group.plants.map((plant, index) => (
-                                <div className="product-card" key={index}>
-                                    <div className="product-title">{plant.name}</div>
-                                    <img src={plant.image} alt={plant.name} className="product-image" />
-                                    <div className="product-description">{plant.cost}</div>
-                                    <div className="product-cost">{plant.description}</div>
+                        <>
+                            <div>
+                                <h1 className="product-category">{group.category}</h1>
+                            </div>
+                            <div className="product-list">
+                                {group.plants.map((plant, index) => (
+                                    <div className="product-card" key={index}>
+                                        <div className="product-title">{plant.name}</div>
+                                        <img src={plant.image} alt={plant.name} className="product-image" />
+                                        <div className="product-description">{plant.cost}</div>
+                                        <div className="product-cost">{plant.description}</div>
 
-                                    <button
-                                        className="product-button"
-                                        onClick={() => handleAddToCart(plant)} 
-                                    >
-                                        Add to Cart
-                                    </button>
-                                </div>
-                            ))}
-                        </div>
+                                        <button
+                                            className={addedToCart[plant.name] ? "product-button added-to-cart" : "product-button"}
+                                            onClick={() => handleAddToCart(plant)}
+                                            disabled={addedToCart[plant.name]} 
+                                        >
+                                            Add to Cart
+                                        </button>
+                                    </div>
+                                ))}
+                            </div>
+                        </>
                     ))}
                 </div>
             ) : (
